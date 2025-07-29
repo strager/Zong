237d9f24991ef91f55f73083828388929696a3cc
strager <s@strager.net>
2025-07-29 10:49:32 -0400

    refactor: goimports

--- loop_test.go
+++ loop_test.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/nalgeon/be"
 	"testing"
+
+	"github.com/nalgeon/be"
 )
 
 // Loop Integration Tests
--- phase3_test.go
+++ phase3_test.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/nalgeon/be"
 	"testing"
+
+	"github.com/nalgeon/be"
 )
 
 // Phase 3 Tests: Advanced Returns
